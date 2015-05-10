82fc30771e338aaf509de6cebf1b288176dc5d85
vladyslav <va_lysenko@ukr.net>
2015-05-10 17:00:53 +0300

    -Boardgames project finished

--- envs/boardgames/dev/boardhames/main/views.py
+++ envs/boardgames/dev/boardhames/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 
+"""Renders index page
+"""
 def home(request):
     return render(request, "main/home.html")
--- envs/boardgames/dev/boardhames/tictactoe/urls.py
+++ envs/boardgames/dev/boardhames/tictactoe/urls.py
@@ -4,4 +4,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('tictactoe.views',
     url(r'^invite$', 'new_invitation', name='tictactoe_invite'),
     url(r'^invitation/(?P<pk>\d+)/$', 'accept_invitation', name='tictactoe_accept_invitation'),
+    url(r'^game/(?P<pk>\d+)/$', 'game_detail', name='tictactoe_game_detail'),
+    url(r'^game/(?P<pk>\d+)/do_move$', 'game_do_move', name='tictactoe_game_do_move'),
+
 )
